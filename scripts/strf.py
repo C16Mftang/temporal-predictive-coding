e971e368bfa494547cd657c2edde4c731ec899e7
Mufeng Tang <tangmufeng424@gmail.com>
2023-10-12 16:13:58 +0100

    debugged shape problem

--- scripts/strf.py
+++ scripts/strf.py
@@ -227,10 +227,11 @@ def main(args):
         elif datapath == 'bar_patches_simple':
             train = get_bar_patches(train_size, seq_len, h, w, simple=True).astype(np.float16)
         else:
-            train = get_nat_movie(datapath, train_size, seq_len).reshape((train_size, -1, h, w))
+            train = get_nat_movie(datapath, train_size, seq_len).reshape((-1, seq_len, h, w))
         
         # whiten the data
-        train = spatiotemporal_whitening(train, args.whitening)[0].reshape((train_size, -1, h, w)).astype(np.float16)
+        if not args.whitening:
+            train = spatiotemporal_whitening(train, args.whitening)[0].reshape((-1, seq_len, h, w)).astype(np.float16)
 
         # make training data a dataloader
         train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
